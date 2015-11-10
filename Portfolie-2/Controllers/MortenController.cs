af61ec7e65648531362ec1606e9316ccaf58bed6
Stefan Meldgaard <steven_meldgaard@hotmail.com>
2015-11-10 12:28:48 +0100

    first commit

---
+++ Portfolie-2/Controllers/MortenController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Portfolie_2.Controllers
+{
+    public class MortenController : ApiController
+    {
+        public string Rope { get; set; }
+    }
+}
---
+++ Portfolie-2/Controllers/StefanController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Portfolie_2.Controllers
+{
+    public class StefanController : ApiController
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
