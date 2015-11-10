f8408b5665bd108f6e0c1b33e09c38227614508d
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-11-10 13:46:08 +0100

    Added UserController

---
+++ Portfolie-2/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Portfolie_2.Controllers
+{
+    public class UserController : ApiController
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Location { get; set; }
+        public string AboutMe { get; set; }
+    }
+}
