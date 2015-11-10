3969acb8a8de7edfde3dad9936eb0581649d6bd6
Stefan Meldgaard <steven_meldgaard@hotmail.com>
2015-11-10 13:53:51 +0100

    post repository, and flow start

---
+++ Portfolie-2/Models/PostModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolie_2.Models
+{
+    public class PostModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Portfolie-2/Repository/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Portfolie_2
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetAll(int limit = 10, int offset = 0)
+        {
+            var sql = string.Format("select id, title, body from posts limit {0} offset {1}", limit, offset);
+
+            foreach (var post in ExecuteQuery(sql))
+                yield return post;
+        }
+
+        private static IEnumerable<Post> ExecuteQuery(string sql)
+        {
+            
+        }
+    }
+}
