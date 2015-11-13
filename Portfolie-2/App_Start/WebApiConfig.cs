927a9ad815e3d069f0b19bb4a2da5fc162023f07
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-13 15:05:08 +0100

    Comment Map route added

--- Portfolie-2/App_Start/WebApiConfig.cs
+++ Portfolie-2/App_Start/WebApiConfig.cs
@@ -31,6 +31,12 @@ namespace Portfolie_2
                 routeTemplate: "api/users/{id}",
                 defaults: new { controller = "User", id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "UserApi",
+                routeTemplate: "api/users/{id}",
+                defaults: new { controller = "Comment", id = RouteParameter.Optional }
+            );
         }
     }
 }
