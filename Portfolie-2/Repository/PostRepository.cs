dd0a48022283f4c862edc84071772326e897e9df
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-11-13 12:34:03 +0100

    Made connectionString more shiny

--- Portfolie-2/Repository/PostRepository.cs
+++ Portfolie-2/Repository/PostRepository.cs
@@ -11,8 +11,15 @@ namespace Portfolie_2.Repository
     {
         public IEnumerable<Post> GetAll(int limit = 10, int offset = 0)
         {
-            using (var connection = new MySqlConnection("server = wt - 220.ruc.dk; database = raw3; uid = raw3; pwd = raw3; port = 3306;"))
-            //(var connection = new MySqlConnection("server=wt-220.ruc.dk;database=raw3;uid=raw3;pwd=raw3;port=3306"))
+            var connectionString = @"Server=wt-220.ruc.dk;
+                                     User ID=raw3;
+                                     Password=raw3;
+                                     Database=raw3;
+                                     Port=3306;
+                                     Pooling=false";
+
+            using (var connection = new MySqlConnection(connectionString))
+           
             {
                 connection.Open();
                 var sql = string.Format("select id, title, body from posts limit {0} offset {1}", limit, offset);
