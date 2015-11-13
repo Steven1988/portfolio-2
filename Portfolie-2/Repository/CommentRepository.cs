1f141859b0a670ca1638264c738b66a1f2486023
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-13 14:29:50 +0100

    Startet on Comment - repository and controller

---
+++ Portfolie-2/Repository/CommentRepository.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using Portfolie_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolie_2.Repository
+{
+    public class CommentRepository
+    {
+        public IEnumerable<Comment> GetAll(int limit = 10, int offset = 0)
+        {
+            var connectionString = @"Server=wt-220.ruc.dk;
+                                     User ID=raw3;
+                                     Password=raw3;
+                                     Database=raw3;
+                                     Port=3306;
+                                     Pooling=false";
+
+            using (var connection = new MySqlConnection(connectionString))
+
+            {
+                connection.Open();
+                var sql = string.Format("select id, title, body from posts limit {0} offset {1}", limit, offset);
+
+                var cmd = new MySqlCommand(sql, connection);
+                using (var rdr = cmd.ExecuteReader())
+                {
+                    // as long as we have rows we can read
+                    while (rdr.HasRows && rdr.Read())
+                    {
+                        yield return new Post
+                        {
+                            Id = rdr.GetInt32(0),
+                            Title = rdr["title"] as string,
+                            Body = rdr["body"] as string
+                        };
+                    }
+                }
+            }
+
+            //var sql = string.Format("select id, title, body from posts limit {0} offset {1}", limit, offset);
+
+            //foreach (var post in ExecuteQuery(sql))
+            //    yield return post;
+        }
+    }
+}
