5ea8bf70e674872203bdd8dede917cf7550d78ac
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-16 22:35:20 +0100

    Tag controller added

---
+++ Portfolie-2/Controllers/TagController.cs
@@ -0,0 +1,26 @@
+﻿using Portfolie_2.Models;
+using Portfolie_2.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Portfolie_2.Controllers
+{
+    public class TagController : ApiController
+    {
+        TagRepository _tagRepository = new TagRepository();
+        public IEnumerable<Tag> Get()
+        {
+            IEnumerable<Tag> p = _tagRepository.GetAll();
+            return p;
+        }
+
+        public Tag Get(int id)
+        {
+            return _tagRepository.GetById(id);
+        }
+    }
+}
