fa176d601d6e40b155af28e23e8e439ca2a4e527
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-17 11:33:39 +0100

    favorite repositry added

--- Portfolie-2/Models/Favorite.cs
+++ Portfolie-2/Models/Favorite.cs
@@ -7,7 +7,7 @@ namespace Portfolie_2.Models
 {
     public class Favorite
     {
-        public int Userd { get; set; }
+        public int UserId { get; set; }
         public int PostId { get; set; }
         public string Annotation { get; set; }
 
---
+++ Portfolie-2/Repository/FavoriteRepository.cs
@@ -0,0 +1,75 @@
+﻿using MySql.Data.MySqlClient;
+using Portfolie_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolie_2.Repository
+{
+    public class FavoriteRepository
+    {
+        public IEnumerable<Favorite> GetAll(int limit = 10, int offset = 0)
+        {
+            var connectionString = @"Server=wt-220.ruc.dk;
+                                     User ID=raw3;
+                                     Password=raw3;
+                                     Database=raw3;
+                                     Port=3306;
+                                     Pooling=false";
+
+            using (var connection = new MySqlConnection(connectionString))
+
+            {
+                connection.Open();
+                var sql = string.Format("select userid, postId, annotation from favorites limit {0} offset {1}", limit, offset);
+
+                var cmd = new MySqlCommand(sql, connection);
+                using (var rdr = cmd.ExecuteReader())
+                {
+                    // as long as we have rows we can read
+                    while (rdr.HasRows && rdr.Read())
+                    {
+                        yield return new Favorite
+                        {
+                            UserId = rdr.GetInt32(0),
+                            PostId = rdr.GetInt32(1),
+                            Annotation = rdr["text"] as string
+                        };
+                    }
+                }
+            }
+        }
+
+        public Favorite GetByUserId(int userId)
+        {
+            var connectionString = @"Server=wt-220.ruc.dk;
+                                     User ID=raw3;
+                                     Password=raw3;
+                                     Database=raw3;
+                                     Port=3306;
+                                     Pooling=false";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                var sql = string.Format("select userid, postid, annotation from favorites where userid = {0}", userId);
+
+                var cmd = new MySqlCommand(sql, connection);
+                using (var rdr = cmd.ExecuteReader())
+                {
+                    if (rdr.HasRows && rdr.Read())
+                    {
+                        return new Favorite
+                        {
+                            UserId = rdr.GetInt32(0),
+                            PostId = rdr.GetInt32(1),
+                            Annotation = rdr["text"] as string
+                        };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
