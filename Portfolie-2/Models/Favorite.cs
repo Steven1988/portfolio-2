9dc6d5ff0321796fdd7e63f6a17ae549da41176c
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-17 11:26:51 +0100

    favorites model added

---
+++ Portfolie-2/Models/Favorite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolie_2.Models
+{
+    public class Favorite
+    {
+        public int Userd { get; set; }
+        public int PostId { get; set; }
+        public string Annotation { get; set; }
+
+    }
+}
