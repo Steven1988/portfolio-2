0252ded60214a1d7f5abbe07ad2c582cea4112a6
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-17 11:40:53 +0100

    favoritecontroller added

---
+++ Portfolie-2/Controllers/FavoriteController.cs
@@ -0,0 +1,28 @@
+﻿using Portfolie_2.Models;
+using Portfolie_2.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Portfolie_2.Controllers
+{
+    public class FavoriteController : ApiController
+    {
+        public class CommentController : ApiController
+        {
+            FavoriteRepository _favoriteRepository = new FavoriteRepository();
+            public IEnumerable<Favorite> Get()
+            {
+                return _favoriteRepository.GetAll();
+            }
+
+            public Favorite Get(int userId)
+            {
+                return _favoriteRepository.GetByUserId(userId);
+            }
+        }
+    }
+}
