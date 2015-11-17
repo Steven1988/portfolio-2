0975459b3009547b7e200d39e5dd511104df2656
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-17 11:43:39 +0100

    favorite apiconfig addded

--- Portfolie-2/App_Start/WebApiConfig.cs
+++ Portfolie-2/App_Start/WebApiConfig.cs
@@ -49,6 +49,12 @@ namespace Portfolie_2
                 routeTemplate: "api/ti/{tid}",
                 defaults: new { controller = "Ti", tid = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+                name: "FavoriteApi",
+                routeTemplate: "api/Favorites/{userId}",
+                defaults: new { controller = "Ti", userId = RouteParameter.Optional }
+            );
         }
     }
 }
