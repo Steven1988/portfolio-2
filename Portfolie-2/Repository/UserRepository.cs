8a219b3802b3856ee66fe04b9044e1a5cb7861f6
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-11-17 11:46:44 +0100

    sodomizing UserController and UserRepository.

--- Portfolie-2/Repository/UserRepository.cs
+++ Portfolie-2/Repository/UserRepository.cs
@@ -5,11 +5,38 @@ using System.Web;
 using MySql.Data.MySqlClient;
 using Portfolie_2.Models;
 
-namespace Portfolie_2
+namespace Portfolie_2.Repository
 {
     public class UserRepository
     {
         public IEnumerable<User> GetAll(int limit = 10, int offset = 0)
+        {
+            var sql = string.Format(@"select 
+                                    Id, 
+                                    DisplayName, 
+                                    CreationDate, 
+                                    Location, 
+                                    AboutMe, 
+                                    Age 
+                                    from users limit {0} offset {1}", limit, offset);
+            foreach (var user in ExecuteQuery(sql))
+                yield return user;
+        }
+
+        public User GetById(int id)
+        {
+            var sql = string.Format(@"select 
+                                    Id, 
+                                    DisplayName, 
+                                    CreationDate, 
+                                    Location, 
+                                    AboutMe, 
+                                    Age 
+                                    from users where id = {0} ", id);
+            return ExecuteQuery(sql).FirstOrDefault();
+        }
+
+        private static IEnumerable<User> ExecuteQuery(string sql)
         {
             var connectionString = @"Server=wt-220.ruc.dk;
                                      User ID=raw3;
@@ -19,10 +46,8 @@ namespace Portfolie_2
                                      Pooling=false";
 
             using (var connection = new MySqlConnection(connectionString))
-
             {
                 connection.Open();
-                var sql = string.Format("select * from users limit {0} offset {1}", limit, offset);
 
                 var cmd = new MySqlCommand(sql, connection);
                 using (var rdr = cmd.ExecuteReader())
@@ -41,62 +66,6 @@ namespace Portfolie_2
                     }
                 }
             }
-
-            //var sql = string.Format("select id, title, body from posts limit {0} offset {1}", limit, offset);
-
-            //foreach (var post in ExecuteQuery(sql))
-            //    yield return post;
         }
-        
-        
-        //public IEnumerable<Models.User> GetAll(int limit = 10, int offset = 0)
-        //{
-        //    var sql = string.Format("select * from comments limit 10 { 0} offset {1}", limit, offset);
-
-        //    foreach (var post in ExecuteQuery(sql))
-        //        yield return post;
-        //}
-
-        //private static IEnumerable<Models.User> ExecuteQuery(string sql)
-        //{
-        //    var connectionString = @"Server=wt-220.ruc.dk;
-        //                             User ID=raw3;
-        //                             Password=raw3;
-        //                             Database=raw3;
-        //                             Port=3306;
-        //                             Pooling=false";
-
-        //    using (var connection = MySqlConnection(connectionString))
-        //    {
-        //        connection.Open();
-
-        //        var cmd = new MySqlCommand(sql, connection);
-
-        //        using (var rdr = cmd.ExecuteQuery())
-        //        {
-        //            // as long as we have rows we can read
-        //            while (rdr.HasRows && rdr.Read())
-        //            {
-        //                yield return new User
-        //                {
-        //                    Id = rdr.GetInt32(0),
-        //                    DisplayName = rdr.GetString(1),
-        //                    CreationDate = rdr.GetDateTime(2),
-        //                    Location = rdr.GetString(3),
-        //                    AboutMe = rdr.GetString(4)
-        //                };
-        //            }
-        //        }
-        //    }
-        //}
-
-        //private class User : Models.User
-        //{
-        //    public object AboutMe { get; set; }
-        //    public object CreationDate { get; set; }
-        //    public object DisplayName { get; set; }
-        //    public object Id { get; set; }
-        //    public object Location { get; set; }
-        //}
     }
 }
