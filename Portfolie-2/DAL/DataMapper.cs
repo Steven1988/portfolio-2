245d73e11274e8d9b4ae96c18dd799d9972cc263
Stefan Meldgaard <steven_meldgaard@hotmail.com>
2015-11-23 12:53:44 +0100

    trying to build FavoriteMapper and Data Mapper

--- Portfolie-2/DAL/DataMapper.cs
+++ Portfolie-2/DAL/DataMapper.cs
@@ -5,7 +5,19 @@ using System.Web;
 
 namespace Portfolie_2.DAL
 {
-    public class DataMapper
+    public abstract class DataMapper<Bobo> : DataMapper<Bobo> where Bobo : class, IIdentityField
     {
+        // The name of the table to be mapped
+        public string TableName { get; set; }
+
+        // The attributes to be mapped from the database entity, except
+        // the Id that is always assumed pressent.
+        public string[] Attributes { get; set; }
+
+
+        public virtual Bobo Find(long id)
+        {
+            var sql = string.Format("select {0} from {1}", Attributes, TableName)
+        }
     }
 }
---
+++ Portfolie-2/DAL/Favorites/FavoriteMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Portfolie_2.Models;
+using MySql.Data.MySqlClient;
+using Portfolie_2.Repository;
+
+namespace Portfolie_2.DAL.Favorites
+{
+    public class FavoriteMapper : DataMapper<Favorite>
+    {
+        public FavoriteMapper(ConnectionString.String) : base(ConnectionString.String)
+        {
+            TableName = "FAVORITES";
+            Attributes = new string[] { "userid", "postId", "annotation" };
+        }
+
+
+    }
+}
