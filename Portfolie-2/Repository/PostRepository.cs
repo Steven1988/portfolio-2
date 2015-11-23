41af069c1b7a64a9534d3c14af887f129e4b2f32
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-23 14:31:33 +0100

    removes holes in time and space. (removed spaces and few comments

--- Portfolie-2/Repository/PostRepository.cs
+++ Portfolie-2/Repository/PostRepository.cs
@@ -48,13 +48,6 @@ namespace Portfolie_2.Repository
                 where (users.id = posts.UserId and posts.Id = {0}) OR  (users.id = posts.UserId and ParentId={0})
                 order by CreationDate asc", id);
 
-            //var connectionString = @"Server=wt-220.ruc.dk;
-            //                         User ID=raw3;
-            //                         Password=raw3;
-            //                         Database=raw3;
-            //                         Port=3306;
-            //                         Pooling=false";
-
             using (var connection = new MySqlConnection(ConnectionString.String))
             {
                 connection.Open();
