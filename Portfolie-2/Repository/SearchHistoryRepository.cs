c0da84a0c63d415ba6b4106c9ca4d51e774d08ff
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-24 15:30:09 +0100

    search history repository. empty methods

---
+++ Portfolie-2/Repository/SearchHistoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Portfolie_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolie_2.Repository
+{
+    public class SearchHistoryRepository : ISearchHistoryRepository
+    {
+        public IEnumerable<SearchHistory> GetByUserId(int UserId, int limit = 10, int offset = 0)
+        {
+            return null;
+        }
+
+
+        public void DeleteAllUserId(int userId)
+        {
+            
+        }
+
+    }
+}
