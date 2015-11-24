64fe22716d1bec6bd3e5fb0db04d9a52c4e56cfd
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-24 16:06:24 +0100

    delete and GetByUserId method in SearchHistoryRepository filled

--- Portfolie-2/Repository/SearchHistoryRepository.cs
+++ Portfolie-2/Repository/SearchHistoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using Portfolie_2.Models;
+﻿using MySql.Data.MySqlClient;
+using Portfolie_2.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,16 +9,55 @@ namespace Portfolie_2.Repository
 {
     public class SearchHistoryRepository : ISearchHistoryRepository
     {
-        public IEnumerable<SearchHistory> GetByUserId(int UserId, int limit = 10, int offset = 0)
+        public IEnumerable<SearchHistory> GetByUserId(int userId, int limit = 10, int offset = 0)
         {
-            return null;
+            var sql = string.Format(@"select 
+                id, query, queryTime, userId
+
+                from searchHistory
+                where userid = {1}
+                limit {2} offset {3}", userId, limit, offset);
+
+            using (var connection = new MySqlConnection(ConnectionString.String))
+            {
+                connection.Open();
+
+                var cmd = new MySqlCommand(sql, connection);
+                using (var rdr = cmd.ExecuteReader())
+                {
+                    // as long as we have rows we can read
+                    while (rdr.HasRows && rdr.Read())
+                    {
+                        var aSearchHistory = new SearchHistory()
+                        {
+                            Id = (int)rdr["id"],
+                            Query = rdr["Query"] as string,
+                            QueryTime = (DateTime)rdr["QueryTime"],
+                            UserId = (int)rdr["UserId"]
+                        };
+                        yield return aSearchHistory;
+                    }
+                }
+            }
         }
 
 
         public void DeleteAllUserId(int userId)
         {
-            
-        }
+            var sql = string.Format(@"select 
+                id, query, queryTime, userId
 
+                from searchHistory
+                where userid = {1}
+                limit {2} offset {3}", userId, limit, offset);
+
+            using (var connection = new MySqlConnection(ConnectionString.String))
+            {
+                connection.Open();
+
+                var cmd = new MySqlCommand(sql, connection);
+                cmd.BeginExecuteNonQuery();
+            }
+        }
     }
 }
