a6adaf07a246023b088f4f4df4bfd83c177866dd
Morten Lau Larsen <mlaul@ruc.dk>
2015-11-24 16:49:03 +0100

    SearchHistoryController added

---
+++ Portfolie-2/Controllers/SearchHistoryController.cs
@@ -0,0 +1,24 @@
+﻿using Portfolie_2.Models;
+using Portfolie_2.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Portfolie_2.Controllers
+{
+    public class SearchHistoryController : ApiController
+    {
+        SearchHistoryRepository _searchHistoryRepository = new SearchHistoryRepository();
+        public IEnumerable<SearchHistory> Get(int userId)
+        {
+            IEnumerable<SearchHistory> p = _searchHistoryRepository.GetByUserId(userId);
+            return p;
+        }
+
+        //public HttpRequestMessage Delete(int )
+
+    }
+}
