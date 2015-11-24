df7abec5f16d1ed10e59e9fdbd2034a21e690868
yolmonima <yolmonima@gmail.com>
2015-11-24 12:20:09 +0100

    nothing special

---
+++ Portfolie-2/Controllers/AnnotationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolie_2.Controllers
+{
+    public class AnnotationController : Controller
+    {
+        // GET: Annotation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
