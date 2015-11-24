3fd839aa66a8035eae732ee6cdd94e0d8b9b8898
yolmonima <yolmonima@gmail.com>
2015-11-24 16:42:38 +0100

    delete works..

--- Portfolie-2/Controllers/FavoriteController.cs
+++ Portfolie-2/Controllers/FavoriteController.cs
@@ -30,19 +30,38 @@ namespace Portfolie_2.Controllers
 
             HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
             response.StatusCode = HttpStatusCode.Created;
-            string uri = Url.Link("DefaultApi", new { userId = fav.UserId });
+            string uri = Url.Link("FavoriteApi", new { userId = fav.UserId });
             response.Headers.Location = new Uri(uri);
 
             return null;
 
         }
 
-        // Update Fav
-        public Favorite Put(int id, [FromBody] Favorite fav)
+        // Create Fav
+        public Favorite Post(int id, [FromBody] Favorite fav)
         {
             _favoriteRepository.Create(fav.UserId, fav.PostId, fav.Annotation);
             return null;
 
         }
+        // Delete annotation
+        public HttpResponseMessage Delete(int userId, int postId)
+        {
+            _favoriteRepository.Delete(userId, postId);
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            return response; 
+
+        }
+        // Update an annotation
+        public HttpResponseMessage put(int userId, int postId, string annotation, [FromBody] Favorite fav)
+        {
+            _favoriteRepository.Update(fav.UserId, fav.PostId, fav.Annotation);
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+
+        }
+
     }
 }
