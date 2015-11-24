e06488fc45f62162ef9827642ee81c02fd0bd2a1
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-11-24 16:48:54 +0100

    Trying trying trying to do unit testing

--- Portfolie2_Test/UnitTest1.cs
+++ Portfolie2_Test/UnitTest1.cs
@@ -10,28 +10,30 @@ using System.Web.Http;
 
 namespace Portfolie2_Test
 {
-    class FakeRepo : UserRepository
-    {
-        public User GetUserById(int id)
-        {
-            return new User { DisplayName = "Peter", Age = 22 };
-        }
-    }
+    //class FakeRepo : UserRepository
+    //{
+    //    public User GetUserById(int id)
+    //    {
+    //        return new User { DisplayName = "Peter", Age = 22 };
+    //    }
+    //}
 
     public class ControllerTests
     {
         [Test]
         public void GetPerson_ValidId_ReturnsPersonAsJson()
         {
+            //Arrange
             var repoMock = new Mock<IUserRepository>();
             repoMock.Setup(m => m.GetById(It.IsAny<int>()))
                 .Returns(new User { DisplayName = "Peter", Age = 22 });
             //repoMock.Setup(m => m.Something).Returns(5);
             var controller = new UserController(repoMock.Object);
 
+            //Act
             var result = controller.GetUser(1);
 
-
+            //Assert
             Assert.AreEqual("{\"DisplayName\":\"Peter\",\"Age\":22}", result);
         }
     }
