345400f7d53b92879f2035c8480b95d67511f498
yolmonima <yolmonima@gmail.com>
2015-11-26 18:18:00 +0100

    url into mapper

--- Portfolie-2/Models/DetailPost.cs
+++ Portfolie-2/Models/DetailPost.cs
@@ -7,7 +7,7 @@ namespace Portfolie_2.Models
 {
     public class DetailPost
     {
-        public string Url { get; set; }
+       
         public int Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
@@ -32,7 +32,7 @@ namespace Portfolie_2.Models
         // Nested User Object.
         public class User
         {
-            public string Url { get; set; }
+            
             public int UserId { get; set; }
             public string Name { get; set; }
         }
@@ -40,7 +40,7 @@ namespace Portfolie_2.Models
         // Nested Comment Object
         public class Comment
         {
-            public string Url { get; set; }
+            
             public int CommentId { get; set; }
             public string Text { get; set; }
             public DateTime CreationDate { get; set; }
