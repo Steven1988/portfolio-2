b53147e3c7efb8691aeb92bcd34f2fbe70fe5026
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-11-26 14:12:55 +0100

    UnitTestUserRepo works

---
+++ Portfolie2_Test/PostRepoTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Moq;
+using Portfolie_2.Models;
+using Portfolie_2.Repository;
+using Portfolie_2.Controllers;
+using System.Web.Http;
+
+namespace Portfolie2_Test
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestFixture]
+    public class PostRepoTest
+    {
+        [Test]
+        public void TestPostRepo()
+        {
+            //Arrange
+            var postRepoTest = new PostRepository();
+
+            //Act
+            //var contains = pfDetails.GetPfEmployerControlSoFar(1);
+            var getPostById = postRepoTest.GetById(28894151, );
+
+            //Assert
+            Assert.That(getPostById,Is.EqualTo(45325), "It's not as expected");
+        }
+
+
+    }
+}
