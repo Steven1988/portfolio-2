7b5d36c89ab45f0ec98c3bc58b91b0147742ef47
Stefan Meldgaard <steven_meldgaard@hotmail.com>
2015-12-09 16:49:16 +0100

    still trying to get backend data to the html page

--- Portfolie-2/app/js/postdetail.js
+++ Portfolie-2/app/js/postdetail.js
@@ -1,9 +1,16 @@
 ﻿define([''], function (ko) {
     function postdetailViewModel() {
         this.MyName = "Stefan";
-        console.log('im from the postdetailVM' + this.MyName);
 
+        var id = 105975;
+        $.getJSON("/api/posts/" + id, function (data) {
+            console.log(data);
+        });
+
+        //console.log(data);
+        //console.log("the id is: " + id)
     }
+
     return postdetailViewModel;
 
 });
---
+++ Portfolie-2/app/js/posts.js
@@ -0,0 +1,17 @@
+﻿define(['Scripts/knockout-3.4.0'], function (ko) {
+    postViewModel = function() {
+      
+        var data = ko.observableArray([]);
+        $.getJSON("/api/posts", function (posts) {
+            
+            data(posts);
+            console.log(posts);
+        });
+        
+        return {
+            posts: data
+        }
+    };
+   
+    return postViewModel;
+})
