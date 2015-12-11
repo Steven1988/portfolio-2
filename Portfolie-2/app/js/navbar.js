ef5c661e6cfaac92a7747220e107d7ab48b843cf
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-12-11 13:58:31 +0100

    Working on navbar and favorites

---
+++ Portfolie-2/app/js/navbar.js
@@ -0,0 +1,31 @@
+﻿define(['Scripts/knockout-3.4.0'], function(ko) {
+    var viewModel = {
+        message: ko.observable("Hello world!")
+    };
+
+    $(function() {
+        ko.applyBinding(viewModel);
+    });
+        
+    //return postViewModel;
+
+    //function X() {
+    //    var hello = ko.observable('uuyutuy');
+    //    var data = ko.observableArray(['a', 'b', 'c']);
+
+    //    $.getJSON("/api/posts", function (posts) {
+            
+    //        data(posts.data);
+    //                console.log(posts);
+    //            });
+
+    //    return {
+    //        hello: hello,
+    //        posts: data
+    //    }
+        
+    //}
+
+    //return X;
+})
+
