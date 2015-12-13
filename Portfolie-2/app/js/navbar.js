4dc695e0c6f712eeaf728963ff800c02102e1040
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-12-13 18:44:33 +0100

    implemented navbar as knockout component.

--- Portfolie-2/app/js/navbar.js
+++ Portfolie-2/app/js/navbar.js
@@ -1,31 +1,8 @@
-﻿define(['Scripts/knockout-3.4.0'], function(ko) {
-    var viewModel = {
-        message: ko.observable("Hello world!")
-    };
-
-    $(function() {
-        ko.applyBinding(viewModel);
+﻿// Here's my data model
+var ViewModel = function () {
+    ko.components.register("navbar-component", {
+        template:
+            "<div class='navbar navbar-inverse navbar-fixed-top' role='navigation'> <div class='container'> <div class='navbar-header'> <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'> <span class='icon-bar'></span> <span class='icon-bar'></span> <span class='icon-bar'></span> </button> <a class='navbar-brand' href='indexWithMenu.html'>SOVA</a> </div><div class='collapse navbar-collapse'> <ul class='nav navbar-nav'> <li class='active'><a href='Views/posts.html'><i class='fa fa-comments'></i> New Posts</a></li><li><a href='Views/tags.html'><i class='fa fa-hashtag'></i> Find By Tags</a></li></ul> <ul class='nav navbar-nav navbar-right'> <li class='dropdown'> <a href='#' class='dropdown-toggle' data-toggle='dropdown'><i class='fa fa-user'></i> User Menu <b class='caret'></b></a> <ul class='dropdown-menu'> <li><a href='Views/favorites.html'>My Favorites</a></li><li><a href='Views/history.html'>My Search History</a></li><li class='divider'></li><li class='dropdown-header'>Nav header</li><li><a href='#'>Separated link</a></li><li><a href='#'>One more separated link</a></li></ul> </li></ul> </div></div></div>"
     });
-        
-    //return postViewModel;
-
-    //function X() {
-    //    var hello = ko.observable('uuyutuy');
-    //    var data = ko.observableArray(['a', 'b', 'c']);
-
-    //    $.getJSON("/api/posts", function (posts) {
-            
-    //        data(posts.data);
-    //                console.log(posts);
-    //            });
-
-    //    return {
-    //        hello: hello,
-    //        posts: data
-    //    }
-        
-    //}
-
-    //return X;
-})
-
+};
+ko.applyBindings(new ViewModel());
