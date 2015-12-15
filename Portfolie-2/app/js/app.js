9f29135fb0ce96b65b8aa3afab85b92b72f11041
Stefan Meldgaard <steven_meldgaard@hotmail.com>
2015-12-15 14:13:53 +0100

    components working the right way

--- Portfolie-2/app/js/app.js
+++ Portfolie-2/app/js/app.js
@@ -1,92 +1,95 @@
 ﻿var app = app || {};
 
-app.mainViewModel = function () {
+require(['knockout','jQuery'], function (ko) {
 
-    var currentComponent = ko.observable("posts");
+    app.mainViewModel = function () {
 
-    return {
-        currentComponent: currentComponent,
-        //change: function () {
-        //    this.currentComponent("postDetail")
-        //}
+        var currentComponent = ko.observable("posts");
+
+        return {
+            currentComponent: currentComponent,
+            //change: function () {
+            //    this.currentComponent("postDetail")
+            //}
+        }
     }
-}
 
 
-ko.components.register('navbarComponent', {
-    viewModel: {
-        createViewModel: navbarVM = function (params) {
-            var menuItems =
-                [{ mItem: "posts" },
-                { mItem: "tags" },
-                { mItem: "postDetail" }];
+    ko.components.register('navbarComponent', {
+        viewModel: {
+            createViewModel: navbarVM = function (params) {
+                var menuItems =
+                    [{ mItem: "posts" },
+                    { mItem: "tags" },
+                    { mItem: "postDetail" }];
 
 
-            var currentComponent = params.selectedComponent;
-            var mItem = ko.observable();
+                var currentComponent = params.selectedComponent;
+                var mItem = ko.observable();
 
-            showContent = function (mItem) {
-                currentComponent(mItem);
-                console.log(mItem);
+                showContent = function (mItem) {
+                    currentComponent(mItem);
+                    console.log(mItem);
+                }
+                return {
+                    menuItems: menuItems,
+                    currentComponent: currentComponent,
+                    showContent: showContent
+                }
             }
-            return {
-                menuItems: menuItems,
-                currentComponent: currentComponent,
-                showContent: showContent
+        },
+        template: { require: 'Scripts/text!Views/navbar.html' }
+    });
+
+    ko.components.register('posts', {
+        viewModel: { require: 'app/js/posts' },
+        template: { require: 'Scripts/text!Views/posts.html' }
+    });
+
+    //ko.components.register('posts', {
+    //    viewModel: {
+    //        createViewModel: postVM = function () {
+    //            var hello = ko.observable("hello my");
+    //            var data = ko.observableArray([]);
+
+    //            $.getJSON("/api/posts", function (posts) {
+
+    //                data(posts.data);
+    //                console.log(posts);
+    //            });
+    //            return {
+    //                hello: hello,
+    //                posts: data
+    //            };
+    //        }
+    //        //require: 'app/js/posts.js'     
+    //    },
+    //    template: { require: 'Scripts/text!Views/posts.html' }
+    //});
+
+    ko.components.register('postDetail', {
+        viewModel: {
+            createViewModel: function () {
+
+                var hello = ko.observable("New hello in PD")
+                var data = ko.observableArray([]);
+                var id = 105568;
+
+                $.getJSON("/api/posts/" + id, function (pd) {
+                    data(pd.data);
+                    console.log(pd.data);
+                });
+                return {
+                    hello: hello,
+                    data: data
+                };
             }
-        }
-    },
-    template: { require: 'Scripts/text!Views/navbar.html' }
+        },
+        template: { require: 'Scripts/text!Views/postdetail.html' }
+    });
+
+
+    ko.applyBindings(new app.mainViewModel())
+    //ko.applyBindings(new postViewModel())
+
 });
-
-ko.components.register('posts', {
-    viewModel: {
-        createViewModel: postVM = function () {
-            var hello = ko.observable("hello my");
-            var data = ko.observableArray([]);
-
-            $.getJSON("/api/posts", function (posts) {
-
-                data(posts.data);
-                console.log(posts);
-            });
-            return {
-                hello: hello,
-                posts: data
-            };
-        }
-        //require: 'app/js/posts.js'     
-    },
-    template: { require: 'Scripts/text!Views/posts.html' }
-});
-
-ko.components.register('postDetail', {
-    viewModel: {
-        createViewModel: function () {
-
-            var hello = ko.observable("New hello in PD")
-            var data = ko.observableArray([]);
-            var id = 105568;
-
-            $.getJSON("/api/posts/" + id, function (pd) {
-                data(pd.data);
-                console.log(pd.data);
-            });
-            return {
-                hello: hello,
-                data: data
-            };
-        }
-    },
-    template: { require: 'Scripts/text!Views/postdetail.html' }
-});
-
-ko.components.register('Tags', {
-    viewModel: { require: 'app/js/tags' },
-    template: { require: 'Scripts/text!Views/tags.html' },
-    synchronous: true
-});
-
-
-ko.applyBindings(new app.mainViewModel())
-//ko.applyBindings(new postViewModel())
--- Portfolie-2/app/js/posts.js
+++ Portfolie-2/app/js/posts.js
@@ -1,40 +1,20 @@
-﻿define(['Scripts/knockout-3.4.0'], function (ko) {
-    //postViewModel = function() {
-      
-    //    var data = ko.observableArray([]);
-    //    $.getJSON("/api/posts", function (posts) {
-            
-    //        data(posts);
-    //        console.log(posts);
-    //    });
-        
-    //    return {
-    //        hello: "hello",
-    //        posts: data
-    //    }
-    //    //console.log(posts);
-
-    //};
-
-    //return postViewModel;
-
-    function X() {
-        var hello = ko.observable('uuyutuy');
-        var data = ko.observableArray(['a', 'b', 'c']);
-
+﻿define(['knockout','jQuery'], function (ko) {
+    postViewModel = function() {
+        var hello = ko.observable('Something new');
+        var data = ko.observableArray([]);
         $.getJSON("/api/posts", function (posts) {
             
             data(posts.data);
-                    console.log(posts);
-                });
-
+            console.log(posts);
+        });
+        
         return {
             hello: hello,
             posts: data
         }
-        
-    }
+        //console.log(posts);
+    };
 
-    return X;
+    return postViewModel;
 })
 
