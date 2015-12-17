157528286fc5411301d583b225d2c068f6736e63
yolmonima <yolmonima@gmail.com>
2015-12-17 13:53:23 +0100

    Search Delete, Css-hide

--- Portfolie-2/app/js/app.js
+++ Portfolie-2/app/js/app.js
@@ -4,7 +4,7 @@ require(['knockout', 'jQuery', 'bootstrap', 'moment'], function (ko) {
 
     app.mainViewModel = function () {
         mainVM = this;
-        var currentComponent = ko.observable("postdetail");
+        var currentComponent = ko.observable("searchhistory");
 
         return {
             currentComponent: currentComponent,
