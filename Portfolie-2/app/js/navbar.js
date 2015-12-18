b3f5331f0199b3f295fe69628e4e3b35e2a14b65
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-12-18 13:53:06 +0100

    Navbar is finally pretty again (icons work)

--- Portfolie-2/app/js/navbar.js
+++ Portfolie-2/app/js/navbar.js
@@ -1,12 +1,13 @@
-﻿define(['knockout', 'jQuery','bootstrap'], function (ko) {
+﻿define(['knockout', 'jQuery', 'bootstrap'], function (ko) {
     navbarVM = function (params) {
-        var menuItems = [
-            { mItem: "Search" },
-            { mItem: "Posts" },
-            { mItem: "Tags" },
-            { mItem: "Postdetail" }
-        ];
 
+        var menuItems = [
+            { mItem: "Search", icon: "<i class='fa fa-search'></i>" },
+            { mItem: "Posts", icon: "<i class='fa fa-envelope'></i>" },
+            { mItem: "Tags", icon: "<i class='fa fa-tags'></i>" },
+            { mItem: "Postdetail", icon: "<i class='fa fa-asterisk'></i>" }
+        ];
+    
         var dropdownMenu = [
             { dropItem: "Favorites", icon: "fa-comments" },
             { dropItem: "Search History", icon: "fa-comments" }
