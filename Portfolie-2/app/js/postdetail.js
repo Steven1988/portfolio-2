37927a90a4c988b6d02c064ba7f19f14943b8318
yolmonima <yolmonima@gmail.com>
2015-12-18 15:53:15 +0100

    commit....

--- Portfolie-2/app/js/postdetail.js
+++ Portfolie-2/app/js/postdetail.js
@@ -3,8 +3,12 @@
         var currentPostId = params.selectedPost;
         console.log(currentPostId);
 
-        var sesUserId = 255;
-        var id = 124462;
+        self.PostId = ko.observable();
+        self.UserId = ko.observable();
+        self.Annotation = ko.observable("");
+
+        //var sesUserId = 255;
+        //var id = 124462;
         var data = ko.observableArray([]);
         if (sesUserId != "") {
             $.getJSON("/api/posts/" + id + "/" + sesUserId, function (pd) {
@@ -16,6 +20,13 @@
             $.getJSON("/api/posts/" + id, function (pd) {
                 data(pd.data);
                 console.log(pd.data);
+            });
+        }
+
+        self.saveUserData = function () {
+
+            $.post("api/Favorites/" + self.UserId() + "/" + self.PostId(), function () {
+                alert("Your data is saved");
             });
         }
 
