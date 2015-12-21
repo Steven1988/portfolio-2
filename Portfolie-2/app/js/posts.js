62831c80386358e357eeb39be37dfe3e1787360b
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-12-21 14:37:27 +0100

    Revert "truncate post text"
    This reverts commit 4d7dec1725e5839093d6c3dfd50079cf9088c975.

--- Portfolie-2/app/js/posts.js
+++ Portfolie-2/app/js/posts.js
@@ -8,28 +8,11 @@
         $.getJSON("/api/posts", function (posts) {
 
             data(posts.data);
-            
             //console.log(posts);
 
             page(posts.paging);
-            console.log(posts);
-            console.log(posts.data[2].Body);
+            console.log(posts.paging);
         });
-
-        //truncateText http://jsfiddle.net/bZEQM/31/
-        ko.bindingHandlers.truncatedText = {
-            update: function (element, valueAccessor, allBindingsAccessor) {
-                var originalText = ko.utils.unwrapObservable(valueAccessor()),
-                    // 10 is a default maximum length
-                    length = ko.utils.unwrapObservable(allBindingsAccessor().maxTextLength) || 20,
-                    truncatedText = originalText.length > length ? originalText.substring(0, length) + "..." : originalText;
-                //$(element).html(truncatedText);
-                // updating text binding handler to show truncatedText
-                ko.bindingHandlers.html.update(element, function () {
-                    return truncatedText;
-                });
-            }
-        };
 
         //******* Paging ********
         nextPage = function (nextUrl) {
@@ -47,6 +30,7 @@
         //var summary = 50;
         //console.log(posts.Body);
 
+
         return {
             posts: data,
             page: page,
@@ -54,6 +38,6 @@
             currentPostId: currentPostId
         }
     };
-
     return postViewModel;
 })
+
