aed58033ea4a388fda1b866315246ae9f7ec6803
QuixoticGitHub <AlexanderMangaard@gmail.com>
2015-12-21 18:57:52 +0100

    implemented the text truncation @ search results

--- Portfolie-2/app/js/search.js
+++ Portfolie-2/app/js/search.js
@@ -41,6 +41,22 @@
 
             return searchItem;
         }
+
+        //truncateText http://jsfiddle.net/bZEQM/31/
+        ko.bindingHandlers.truncatedText = {
+            update: function (element, valueAccessor, allBindingsAccessor) {
+                var originalText = ko.utils.unwrapObservable(valueAccessor()),
+                    // 10 is a default maximum length
+                    length = ko.utils.unwrapObservable(allBindingsAccessor().maxTextLength) || 20,
+                    truncatedText = originalText.length > length ? originalText.substring(0, length) + "..." : originalText;
+                //$(element).html(truncatedText);
+                // updating text binding handler to show truncatedText
+                ko.bindingHandlers.html.update(element, function () {
+                    return truncatedText;
+                });
+            }
+        };
+
         return {
             searchPosts: data,
             page: page,
