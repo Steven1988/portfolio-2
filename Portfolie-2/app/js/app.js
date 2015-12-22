c031ed03b849186d733062a86aeaec028fbbe2cc
yolmonima <yolmonima@gmail.com>
2015-12-22 12:38:21 +0100

    footer and dynamic date.

--- Portfolie-2/app/js/app.js
+++ Portfolie-2/app/js/app.js
@@ -8,6 +8,7 @@ require(['knockout', 'jQuery', 'bootstrap', 'moment'], function (ko) {
         var currentComponent = ko.observable("search");
         currentPostId = ko.observable("");
 
+
         //**** setting the sesUserId ******
         var theUser = ko.observable();
         var desiredUserId = 136;
@@ -20,54 +21,81 @@ require(['knockout', 'jQuery', 'bootstrap', 'moment'], function (ko) {
             currentComponent("postdetail");
         }
 
+        //currentDate = function() {
+        //    document.write(new Date().getFullYear())
+        //};
+        //console.log(currentDate)
+
+
+        var mydate = new Date().getFullYear()
+        console.log(mydate)
+
         return {
             currentComponent: currentComponent,
             currentPostId: currentPostId,
-            user: theUser
+            user: theUser,
+            currentDate: mydate
             //change: function () {
             //    this.currentComponent("postDetail")
             //}
         }
     }
 
+
     //************ All of our components **************
     ko.components.register('navbarComponent', {
-        viewModel: { require: 'app/js/navbar' },
-        template: { require: 'Scripts/text!Views/navbar.html' }
-    });
+            viewModel: { require: 'app/js/navbar'
+    },
+            template: { require: 'Scripts/text!Views/navbar.html'
+    }
+});
 
     ko.components.register('posts', {
-        viewModel: { require: 'app/js/posts' },
-        template: { require: 'Scripts/text!Views/posts.html' }
-    });
+            viewModel: { require: 'app/js/posts'
+    },
+            template: { require: 'Scripts/text!Views/posts.html'
+    }
+});
 
     ko.components.register('tags', {
-        viewModel: { require: 'app/js/tags' },
-        template: { require: 'Scripts/text!Views/tags.html' }
-    });
+            viewModel: { require: 'app/js/tags'
+    },
+            template: { require: 'Scripts/text!Views/tags.html'
+    }
+});
 
     ko.components.register('searchhistory', {
-        viewModel: { require: 'app/js/searchHistory' },
-        template: { require: 'Scripts/text!Views/history.html' }
-    });
+            viewModel: { require: 'app/js/searchHistory'
+    },
+            template: { require: 'Scripts/text!Views/history.html'
+    }
+});
 
     ko.components.register('favorites', {
-        viewModel: { require: 'app/js/annotation1' },
-        template: { require: 'Scripts/text!Views/fav.html' }
-    });
+            viewModel: { require: 'app/js/annotation1'
+    },
+            template: { require: 'Scripts/text!Views/fav.html'
+    }
+});
     ko.components.register('postdetail', {
-        viewModel: { require: 'app/js/postdetail' },
-        template: { require: 'Scripts/text!Views/postdetail.html' }
-    });
+            viewModel: { require: 'app/js/postdetail'
+    },
+            template: { require: 'Scripts/text!Views/postdetail.html'
+    }
+});
 
     ko.components.register('search', {
-        viewModel: { require: 'app/js/search' },
-        template: { require: 'Scripts/text!Views/search.html' }
-    });
+            viewModel: { require: 'app/js/search'
+    },
+            template: { require: 'Scripts/text!Views/search.html'
+    }
+});
     ko.components.register('yourprofile', {
-        viewModel: { require: 'app/js/user' },
-        template: { require: 'Scripts/text!Views/user.html'}
-    })
+            viewModel: { require: 'app/js/user'
+    },
+            template: { require: 'Scripts/text!Views/user.html'
+    }
+})
 
 
     ko.applyBindings(new app.mainViewModel());
